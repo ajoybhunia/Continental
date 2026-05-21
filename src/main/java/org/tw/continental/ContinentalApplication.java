d2b8bf6e1c33bbbc35ed34fa7ad3bd42dab750a1
Ajoy Bhunia <ajoybhunia@MacBook-Pro-Ajoy.local>
2026-05-21 11:00:15 +0530

    initial commit

---
+++ src/main/java/org/tw/continental/ContinentalApplication.java
@@ -0,0 +1,13 @@
+package org.tw.continental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tw/continental/ContinentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tw.continental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
